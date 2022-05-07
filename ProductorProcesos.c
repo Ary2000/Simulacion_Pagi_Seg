db01d97c4bc14db34f99a7f717b66dd095c21bc8
Isaac <irsacfac@gmail.com>
2022-05-06 22:17:46 -0600

    Proceso y creador de roceso

---
+++ ProductorProcesos.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+typedef struct{
+   int cantElementos, tiempoEjecucion;
+   int info[];
+
+}proceso;
+
+bool paginacion = true;
+
+proceso crearProceso(){
+    proceso dato = malloc(sizeof(proceso));
+    int elementos;
+    if(paginacion){
+        elementos = rand() % (10) + 1;
+        int espacios[] = {1};
+        dato->cantElementos = elementos;
+        dato->info = espacios
+    }else{
+        elementos = rand() % (5) + 1;
+        int espacios[elementos];
+        for (int i = 0; i < elementos; i++)
+        {
+            espacios[i] = rand() % (3) + 1;
+        }
+        dato->cantElementos = elementos;
+        dato->info = espacios
+    }
+    dato->tiempoEjecucion = rand() % (20) + 1;
+}
