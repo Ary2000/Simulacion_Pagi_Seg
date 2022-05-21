1062c8c2f1774b2b0505da90a4992f8d037c5cd6
Isaac <irsacfac@gmail.com>
2022-05-20 19:15:00 -0600

    Update bitacora

--- Bitacora.c
+++ Bitacora.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include "VariablesGlobales.c"
+#include "listaProcesos.c"
 
 FILE *archivo;
 char fileName[] = "Bitacora.txt";
@@ -44,26 +44,56 @@ void getEstado(char* string, enum estados estado){
     }
 }
 
-void getPosiciones(char* string, proceso *p){
-
+//https://stackoverflow.com/questions/30234363/how-can-i-convert-an-int-array-into-a-string-array
+void getPosiciones(char* string, proceso *p, enum estados estado){
+    int tamanio = p->cantElementos * 5;
+    char posiciones[tamanio];
+    int indice = 0;
+    switch (estado)
+    {
+    case EnMemoria:
+        for (int i = 0; i < p->cantElementos-1; i++)
+        {
+            indice += snprintf(&posiciones[indice], tamanio-indice, "%d", p->registroBase[i]);
+            indice += snprintf(&posiciones[indice], tamanio-indice, "%s", ",");
+        }
+        indice += snprintf(&posiciones[indice], tamanio-indice, "%d", p->registroBase[p->cantElementos-1]);
+        sprintf(string, "Direcciones en memoria: [%s], ",posiciones);
+        break;
+    
+    default:
+        break;
+    }
+    /*int tamanio = p->cantElementos * 5;
+    char posiciones[tamanio];
+    int indice = 0;
+    for (int i = 0; i < p->cantElementos-1; i++)
+    {
+        indice += snprintf(&posiciones[indice], tamanio-indice, "%d", p->registroBase[i]);
+        indice += snprintf(&posiciones[indice], tamanio-indice, "%s", ",");
+    }
+    indice += snprintf(&posiciones[indice], tamanio-indice, "%d", p->registroBase[p->cantElementos-1]);
+    sprintf(string, "Direcciones en memoria: [%s], ",posiciones);*/
 }
 
 void escribirBitacora(proceso *p){
     //PID, estado, espacio en memoria(si esta en memoria)/eliminado, hora
     time_t horaActual = time(NULL);
-    char entrada[90];
-    char estado[40];
-    char pocisionesMem[30];
+    char entrada[150];
+    char estado[50];
+    char pocisionesMem[40];
     getEstado(estado, p->estado);
-    if(p->estado == EnMemoria){
+    /*if(p->estado == EnMemoria){
         getPosiciones(pocisionesMem, p);
-    }
-    sprintf(entrada, "PID: %i, Estado: %s, Hora: %s\n",p->id, estado, ctime(&horaActual));
-    printf(entrada);
+    }*/
+    getPosiciones(pocisionesMem, p, p->estado);
+    sprintf(entrada, "PID: %i, Estado: %s, %s Hora: %s\n",p->id, estado, pocisionesMem, ctime(&horaActual));
+    fprintf(archivo, entrada);
+    //printf(entrada);
 }
 
-/*int main(){
-    int i = 10;
+int main(){
+    /*int i = 10;
     time_t horaActual = time(NULL);
     char e[] = "buscando memoria";
     char PID[10];
@@ -79,6 +109,36 @@ void escribirBitacora(proceso *p){
     sprintf(entrada, "PID: %i, Estado: %s, Hora: %s\n",i, e, ctime(&horaActual));
     printf(entrada); 
     abrirBitacora();
+    cerrarBitacora();*/
+    /*int numeros[] = {2,4,6,18,49};
+    int tamanio = 5*4;
+    char posiciones[tamanio];
+    int indice = 0;
+    for (int i = 0; i < 5-1; i++)
+    {
+        indice += snprintf(&posiciones[indice], tamanio-indice, "%d", numeros[i]);
+        indice += snprintf(&posiciones[indice], tamanio-indice, "%s", ",");
+    }
+    indice += snprintf(&posiciones[indice], tamanio-indice, "%d", numeros[5-1]);
+    printf(posiciones);
+    printf("\n");*/
+    abrirBitacora();
+    proceso* p = malloc(sizeof(p));
+    int elementos = 3;
+    enum estados e = EnMemoria;
+    p->id=4;
+    p->estado = e;
+    p->cantElementos = elementos;
+    p->espacioElementos = calloc(elementos,sizeof(int));
+    p->registroBase = calloc(elementos,sizeof(int));
+    for (int i = 0; i < elementos; i++)
+    {
+        p->espacioElementos[i] = 1;
+    }
+    escribirBitacora(p);
+    e = Saliendo;
+    p->estado = e;
+    escribirBitacora(p);
     cerrarBitacora();
     return 0;
-}*/
+}
